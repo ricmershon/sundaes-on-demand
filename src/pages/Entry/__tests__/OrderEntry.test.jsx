2413bd6745a8639b7b98a6f74c16ba80414d2e78
Ric Mershon <rmershon@tsys.com>
2022-06-20 13:18:17 -0400

    More tests

--- src/pages/Entry/__tests__/OrderEntry.test.jsx
+++ src/pages/Entry/__tests__/OrderEntry.test.jsx
@@ -13,10 +13,10 @@ describe('OrderEntry component', () => {
 
     test('Handles errors for scoops and toppings routes', async () => {
         server.resetHandlers(
-            rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+            rest.get('http://localhost:3030/scoops', (_req, res, ctx) =>
                 res(ctx.status(500))
             ),
-            rest.get('http://localhost:3030/toppings', (req, res, ctx) => 
+            rest.get('http://localhost:3030/toppings', (_req, res, ctx) => 
                 res(ctx.status(500))
             )
         );
